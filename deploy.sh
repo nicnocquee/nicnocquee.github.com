e71274443f088608030bd30dfb2538a6b68018be
Nico Prananta <nicnocquee@users.noreply.github.com>
2017-09-07 11:02:35 +0200

    publish: use deploy.sh
    generated from commit ba6834cea4d447ad1c3c79e49f31fb11e47959dc

--- deploy.sh
+++ deploy.sh
@@ -74,7 +74,7 @@ parse_args() {
 	deploy_branch="master"
 
 	#if no user identity is already set in the current git environment, use this:
-	default_username=${GIT_DEPLOY_USERNAME:-deploy.sh}
+	default_username="nicnocquee"
 	default_email=${GIT_DEPLOY_EMAIL:-}
 
 	#repository to deploy to. must be readable and writable.
